3e82681d054add0f6785e869fa00b49041d9a6c2
Lars <LarsCelie@gmail.com>
2014-12-12 16:03:57 +0100

    DAO finished

--- src/DAO/DAO.java
+++ src/DAO/DAO.java
@@ -11,6 +11,5 @@ import DButil.DBcon;
 public interface DAO {
 	DBcon target = null;
 	DBcon tool = null;
-	public void fetch();
 	public ResultSet fetch(Object o) throws FileNotFoundException, InvalidPropertiesFormatException, IOException, SQLException;
 }
--- src/DAO/DAOValue.java
+++ src/DAO/DAOValue.java
@@ -2,21 +2,40 @@ package DAO;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.InvalidPropertiesFormatException;
 
-public class DAOValue implements DAO{
-	
+import DButil.DBcon;
+import DButil.Oraclecon;
 
-	public void fetch() {
-		throw new UnsupportedOperationException();
-	}
+public class DAOValue implements DAO{
 
 	@Override
 	public ResultSet fetch(Object o) throws FileNotFoundException,
 			InvalidPropertiesFormatException, IOException, SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String name = (String)o;
+		DBcon db = new Oraclecon();
+		Connection con = db.getConnection();
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM value WHERE businessrulenaam=" + name);
+		con.close();
+		return rs;
 	}
+
+	
+	/*
+	 * 
+	 * 	public ResultSet fetch(Object o) throws FileNotFoundException, InvalidPropertiesFormatException, IOException, SQLException {
+		String name = (String)o;
+		DBcon db = new Oraclecon();
+		Connection con = db.getConnection();
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM Businessrule WHERE name=" + name);
+		con.close();
+		return rs;
+	}
+	 */
 }
